97e19552b2f59edd783dc957689e365b0a715db5
shimeoki <shimeoki@gmail.com>
2024-12-14 12:11:49 +0300

    build: add module-info imports from build script

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,2 +1,6 @@
 module io.github.nonmilk.coffee {
+    requires io.github.shimeoki.jfx.rasterization;
+    requires io.github.shimeoki.jshaper;
+    // requires io.github.traunin.triangulation; // TODO
+    requires io.github.alphameo.linear_algebra;
 }
