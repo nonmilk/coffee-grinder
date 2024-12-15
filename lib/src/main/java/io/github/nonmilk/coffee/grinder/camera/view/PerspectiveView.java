2843123a72028d377f4b052b5dd35058d857ef9a
shimeoki <shimeoki@gmail.com>
2024-12-15 17:30:17 +0300

    feat(PerspectiveView): make as a class

--- lib/src/main/java/io/github/nonmilk/coffee/grinder/camera/view/PerspectiveView.java
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/camera/view/PerspectiveView.java
@@ -1,15 +1,20 @@
 package io.github.nonmilk.coffee.grinder.camera.view;
 
+// FIXME javadoc
+
 /**
  * "Lens" parameters for a perspective camera.
  * 
  * @param fov         field of view in radians. Must be in (0; 2pi)
  * @param aspectRatio width / height ratio. Must be positive
  */
-public record PerspectiveView(float fov, float aspectRatio) {
+public final class PerspectiveView {
+
+    private float fov;
+    private float aspectRatio;
 
     /**
-     * Constructs a {@code PerspectiveView} record with the specified field of view
+     * Constructs a {@code PerspectiveView} with the specified field of view
      * and aspect ratio.
      *
      * @param fov         field of view in radians. Must be in (0; 2pi)
@@ -17,12 +22,32 @@ public record PerspectiveView(float fov, float aspectRatio) {
      * @throws IllegalArgumentException If {@code fov} is not in (0; 2pi) or
      *                                  {@code aspectRatio} is not positive
      */
-    public PerspectiveView {
+    public PerspectiveView(final float fov, final float aspectRatio) {
+        setFOV(fov);
+        setAspectRatio(aspectRatio);
+    }
+
+    public float fov() {
+        return fov;
+    }
+
+    public void setFOV(final float fov) {
         if (fov <= 0 || fov >= Math.TAU) {
             throw new IllegalArgumentException("fov has to be in (0; 2pi)");
         }
+
+        this.fov = fov;
+    }
+
+    public float aspectRatio() {
+        return aspectRatio;
+    }
+
+    public void setAspectRatio(final float aspectRatio) {
         if (aspectRatio <= 0) {
             throw new IllegalArgumentException("aspect ratio has to be positive");
         }
+
+        this.aspectRatio = aspectRatio;
     }
 }
