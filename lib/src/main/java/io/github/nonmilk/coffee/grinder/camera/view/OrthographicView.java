0b0f40d8d1493d8fbaf44639a43725863d415122
shimeoki <shimeoki@gmail.com>
2024-12-15 17:37:28 +0300

    feat(OrthographicView): make as a class

--- lib/src/main/java/io/github/nonmilk/coffee/grinder/camera/view/OrthographicView.java
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/camera/view/OrthographicView.java
@@ -1,29 +1,52 @@
 package io.github.nonmilk.coffee.grinder.camera.view;
 
+// FIXME javadoc
+
 /**
  * The dimensions of an orthographic projection.
  *
  * @param width  screen width. Must be positive
  * @param height screen height. Must be positive
  */
-public record OrthographicView(float width, float height) {
+public final class OrthographicView {
+
+    private float width;
+    private float height;
 
     /**
-     * Constructs an {@code OrthographicView} record with the specified width and
+     * Constructs an {@code OrthographicView} with the specified width and
      * height.
      *
      * @param width  screen width. Must be positive
      * @param height screen height. Must be positive
      * @throws IllegalArgumentException If width or height is not positive
      */
-    public OrthographicView {
+    public OrthographicView(final float width, final float height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public float width() {
+        return width;
+    }
+
+    public void setWidth(final float width) {
         if (width <= 0) {
             throw new IllegalArgumentException("Width has to be more than zero");
         }
 
+        this.width = width;
+    }
+
+    public float height() {
+        return height;
+    }
+
+    public void setHeight(final float height) {
         if (height <= 0) {
             throw new IllegalArgumentException("Height has to be more than zero");
         }
-    }
 
+        this.height = height;
+    }
 }
