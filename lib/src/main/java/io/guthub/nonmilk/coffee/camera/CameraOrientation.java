a87cf3a1cdfa71a570d3aafad0f1e9a52df04847
Traunin <traunin5@gmail.com>
2024-12-15 03:32:45 +0300

    feat(CameraOrientation): camera orientation settings

---
+++ lib/src/main/java/io/guthub/nonmilk/coffee/camera/CameraOrientation.java
@@ -0,0 +1,26 @@
+package io.guthub.nonmilk.coffee.camera;
+
+import io.github.alphameo.linear_algebra.vec.Vec3Math;
+import io.github.alphameo.linear_algebra.vec.Vector3;
+import io.guthub.nonmilk.coffee.math.Floats;
+
+/**
+ * A camera's position in 3D space and the direction it is pointing towards.
+ * @param position {@code Vector3} representing camera position
+ * @param target {@code Vector3} a position looked at by the camera
+ */
+public record CameraOrientation(Vector3 position, Vector3 target) {
+
+    /**
+     * Constructs a {@code ClippingBox} record with the specified distances to near and far plane.
+     * @param position {@code Vector3} representing camera position
+     * @param target {@code Vector3} a position looked at by the camera
+     * @throws IllegalArgumentException If target and position match
+     */
+    public CameraOrientation {
+        // copy vectors to break the link?
+        if (Floats.equals(Vec3Math.len2(Vec3Math.subtracted(position, target)), 0)) {
+            throw new RuntimeException("Camera target vector equals camera position");
+        }
+    }
+}
