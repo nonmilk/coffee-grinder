11053d4e1d7eb2ba190c5c3b1dc6817ee9db25ee
Traunin <traunin5@gmail.com>
2024-12-15 03:47:48 +0300

    feat(OrthographicCamera): parallel projection camera

---
+++ lib/src/main/java/io/guthub/nonmilk/coffee/camera/OrthographicCamera.java
@@ -0,0 +1,75 @@
+package io.guthub.nonmilk.coffee.camera;
+
+import io.github.alphameo.linear_algebra.mat.Matrix4;
+
+/**
+ * Represents an orthographic camera.
+ * <p>This camera is defined by the width and height of the produced image, enabling it
+ * to render objects with parallel projection.
+ */
+public final class OrthographicCamera extends Camera {
+
+    private float width;
+    private float height;
+
+    /**
+     * Initializes a new {@code OrthographicCamera} with the specified orientation, screen size and clipping box.
+     *
+     * @param orientation the camera's orientation, defining its position and target in 3D space
+     * @param orthographic the screen settings: width and height
+     * @param clipping the clipping box defining the near and far clipping planes
+     */
+    public OrthographicCamera(CameraOrientation orientation, OrthographicView orthographic, ClippingBox clipping) {
+        super(orientation, clipping);
+        this.width = orthographic.width();
+        this.height = orthographic.height();
+    }
+
+    /**
+     * Gets the camera view width.
+     *
+     * @return {@code float} view width
+     */
+    public float width() {
+        return width;
+    }
+
+    /**
+     * Sets the camera view width.
+     *
+     * @param newWidth {@code float} view width
+     */
+    public void setWidth(final float newWidth) {
+        if (newWidth <= 0) {
+            return;
+        }
+        this.width = newWidth;
+    }
+
+    /**
+     * Gets the camera view height.
+     *
+     * @return {@code float} view height
+     */
+    public float height() {
+        return height;
+    }
+
+    /**
+     * Sets the camera field of view.
+     *
+     * @param newHeight {@code float} view height
+     */
+    public void setHeight(final float newHeight) {
+        if (newHeight <= 0) {
+            return;
+        }
+        this.height = newHeight;
+    };
+
+    @Override
+    public Matrix4 getProjectionMatrix() {
+        // TODO: cache? updates with changes to width, height, clipping planes
+        return CameraMatrix.orthographicProjection(this);
+    }
+}
