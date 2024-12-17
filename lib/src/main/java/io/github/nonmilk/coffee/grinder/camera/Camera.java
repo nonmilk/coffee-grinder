209b630043cf22b8cbfdbdf715f69b7afd0ebc46
Traunin <traunin5@gmail.com>
2024-12-17 13:30:19 +0300

    feat(Camera): camera interface

--- lib/src/main/java/io/github/nonmilk/coffee/grinder/camera/Camera.java
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/camera/Camera.java
@@ -2,72 +2,23 @@ package io.github.nonmilk.coffee.grinder.camera;
 
 import io.github.alphameo.linear_algebra.mat.Matrix4;
 
-public abstract class Camera {
-
-    private CameraOrientation cameraOrientation;
-    private ClippingBox clippingBox;
-
-    /**
-     * Initializes a {@code Camera} object with the specified orientation and
-     * clipping box parameters.
-     *
-     * @param cameraOrientation An instance of {@code CameraOrientation} that
-     *                          defines the camera's
-     *                          position in 3D space and the direction it is
-     *                          pointing towards.
-     * @param clippingBox       {@code ClippingBox} that specifies the near and far
-     *                          clipping planes.
-     */
-    Camera(final CameraOrientation cameraOrientation, final ClippingBox clippingBox) {
-        this.cameraOrientation = cameraOrientation;
-        this.clippingBox = clippingBox;
-    }
+/**
+ * An interface describing a camera in 3D space. It can return it's projection
+ * matrix.
+ */
+public interface Camera extends Clippable {
 
     /**
-     * Gets the camera orientation.
-     *
-     * @return {@code CameraOrientation} representing camera
-     * position and target direction in 3D space
+     * Returns a camera's projection matrix.
+     * 
+     * @return {@code Matrix4} camera's projection matrix
      */
-    public CameraOrientation cameraOrientation() {
-        return cameraOrientation;
-    }
+    public Matrix4 projection();
 
     /**
-     * Sets the camera orientation.
-     *
-     * @param newOrientation {@code CameraOrientation} representing camera
-     * position and target direction in 3D space
+     * Returns a camera's orientation.
+     * 
+     * @return {@code Orientation} camera's orientation
      */
-    public void setCameraOrientation(final CameraOrientation newOrientation) {
-        this.cameraOrientation = newOrientation;
-    }
-
-    public ClippingBox clippingBox() {
-        return clippingBox;
-    }
-
-    public void setClippingBox(final ClippingBox newClippingBox) {
-        this.clippingBox = newClippingBox;
-    }
-
-    /**
-     * Gets the camera view matrix,
-     * which transforms world coordinates into camera or view space.
-     *
-     * @return {@code Matrix} representing camera view matrix
-     */
-    public Matrix4 getViewMatrix() {
-        // TODO: cache? updates with changes to position, target
-        return CameraMatrix.lookAt(this);
-    };
-
-    /**
-     * Gets the camera projection matrix,
-     * which transforms 3D camera or view space coordinates into 2D screen
-     * coordinates
-     *
-     * @return {@code Matrix} representing camera projection matrix
-     */
-    public abstract Matrix4 getProjectionMatrix();
+    public Orientation orientation();
 }
