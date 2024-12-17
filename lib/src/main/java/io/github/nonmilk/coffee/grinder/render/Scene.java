60260f7d848738995928de0b65b6a7ea236d8261
Traunin <traunin5@gmail.com>
2024-12-17 22:13:21 +0300

    feat: Scene setup

---
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/render/Scene.java
@@ -0,0 +1,20 @@
+package io.github.nonmilk.coffee.grinder.render;
+
+import io.github.nonmilk.coffee.grinder.camera.Camera;
+import java.util.ArrayList;
+import java.util.List;
+
+// FIXME write scene
+public class Scene {
+    
+    private final List<Camera> cameras = new ArrayList<>();
+
+
+    public void addModel() {
+
+    }
+
+    public List<Camera> cameras() {
+        return cameras;
+    }
+}
