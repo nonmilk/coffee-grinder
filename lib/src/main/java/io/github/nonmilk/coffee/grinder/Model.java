55201d6cd9193019fae7fdb517772053a892e153
Traunin <traunin5@gmail.com>
2024-12-18 00:19:06 +0300

    feat: normal recalculation setup

--- lib/src/main/java/io/github/nonmilk/coffee/grinder/Model.java
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/Model.java
@@ -7,27 +7,58 @@ import io.github.shimeoki.jshaper.obj.geom.ObjTextureVertex;
 import io.github.shimeoki.jshaper.obj.geom.ObjVertex;
 import io.github.shimeoki.jshaper.obj.geom.ObjVertexNormal;
 import io.github.traunin.triangulation.Triangulation;
+import io.github.alphameo.linear_algebra.vec.Vec3;
+import io.github.alphameo.linear_algebra.vec.Vec3Math;
+import io.github.alphameo.linear_algebra.vec.Vector3;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Model {
 
-    private List<ObjVertex> vertices;
-    private List<ObjTextureVertex> textureVertices;
-    private List<ObjFace> faces;
-    private List<ObjVertexNormal> normals;
+    private final List<ObjVertex> vertices;
+    private final List<ObjTextureVertex> textureVertices;
+    private final List<ObjFace> faces;
+    private final List<ObjVertexNormal> normals;
 
     public Model(ObjFile obj) {
         vertices = obj.vertexData().vertices();
+        final int vertexCount = vertices.size();
         textureVertices = obj.vertexData().textureVertices();
+        // FIXME recalculate normals
+        normals = obj.vertexData().vertexNormals();
         faces = obj.elements().faces();
+
+        final List<Integer> vertexPolygonCount = new ArrayList<>(vertexCount);
+
         for (ObjFace face : faces) {
-            if (face.triplets().size() > 3) {
-                triangulateFace(face);
-            }
+            // addFaceNormals(face, vertexPolygonCount);
+
         }
     }
 
-    private void triangulateFace(ObjFace face) {
-        
+    private void clearNormals() {
+        for (ObjVertexNormal normal : normals) {
+            normal.setI(0);
+            normal.setJ(0);
+            normal.setK(0);
+        }
+    }
+
+    private void addFaceNormals(ObjFace face, List<Integer> vertexPolygonCount) {
+        final ObjVertex vertex1 = face.triplets().get(0).vertex();
+        final ObjVertex vertex2 = face.triplets().get(1).vertex();
+        final ObjVertex vertex3 = face.triplets().get(2).vertex();
+
+        final Vector3 vector1 = new Vec3(vertex1.x(), vertex1.y(), vertex1.z());
+        final Vector3 vector2 = new Vec3(vertex2.x(), vertex2.y(), vertex2.z());
+        final Vector3 vector3 = new Vec3(vertex3.x(), vertex3.y(), vertex3.z());
+
+        final Vector3 edge1 = Vec3Math.subtracted(vector2, vector1);
+        final Vector3 edge2 = Vec3Math.subtracted(vector3, vector2);
+
+        final Vector3 faceNormal = Vec3Math.cross(edge1, edge2);
+
+        // FIXME sum all face normals with vertex and divide by face count
     }
 }
