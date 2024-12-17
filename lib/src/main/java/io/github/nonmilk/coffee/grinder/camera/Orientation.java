20795ba338907161ab4357a088440368de2a95c1
Traunin <traunin5@gmail.com>
2024-12-17 13:31:25 +0300

    refactor(CameraOrientation)!: rename to Orientation

---
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/camera/Orientation.java
@@ -0,0 +1,130 @@
+package io.github.nonmilk.coffee.grinder.camera;
+
+import java.util.Objects;
+
+import io.github.alphameo.linear_algebra.mat.Mat4;
+import io.github.alphameo.linear_algebra.mat.Matrix4;
+import io.github.alphameo.linear_algebra.mat.Matrix4Col;
+import io.github.alphameo.linear_algebra.mat.Matrix4Row;
+import io.github.alphameo.linear_algebra.vec.Vec3;
+import io.github.alphameo.linear_algebra.vec.Vec3Math;
+import io.github.alphameo.linear_algebra.vec.Vector3;
+import io.github.nonmilk.coffee.grinder.math.Floats;
+
+/**
+ * A class holding camera's position in 3D space and the direction it is
+ * pointing towards.
+ */
+public class Orientation {
+
+    private static final Vector3 VECTOR_I = new Vec3(1, 0, 0);
+    private static final Vector3 VECTOR_J = new Vec3(0, 1, 0);
+
+    private Vector3 position;
+    private Vector3 target;
+
+    private final Matrix4 view = new Mat4();
+
+    {
+        view.set(Matrix4Row.R3, Matrix4Col.C3, 1);
+    }
+
+    /**
+     * Initializes a {@code ClippingBox} record with the specified distances to near
+     * and far plane.
+     *
+     * @param position {@code Vector3} representing camera position
+     * @param target   {@code Vector3} a position looked at by the camera
+     * @throws IllegalArgumentException If target and position match
+     */
+    public Orientation(final Vector3 position, final Vector3 target) {
+        checkOverlap(Objects.requireNonNull(position), Objects.requireNonNull(target));
+        this.position = position;
+        this.target = target;
+    }
+
+    public Vector3 position() {
+        return position;
+    }
+
+    public void setPosition(final Vector3 position) {
+        checkOverlap(Objects.requireNonNull(position), target);
+        this.position = position;
+    }
+
+    public Vector3 target() {
+        return target;
+    }
+
+    public void setTarget(final Vector3 target) {
+        checkOverlap(position, Objects.requireNonNull(target));
+        this.target = target;
+    }
+
+    public Vector3 lookDir() {
+        return Vec3Math.subtracted(target, position);
+    }
+
+    private void checkOverlap(final Vector3 position, final Vector3 target) {
+        final Vector3 delta = Vec3Math.subtracted(position, target);
+        if (Floats.equals(Vec3Math.len2(delta), 0)) {
+            throw new IllegalArgumentException("Camera target vector equals camera position");
+        }
+    }
+
+    /**
+     * Updates the view matrix.
+     *
+     * @param camera {@code Camera} child class
+     * @return a camera's look at matrix
+     */
+    public void lookAt() {
+        final Vector3 cameraZ = lookDir();
+
+        final float cameraZLen = Vec3Math.len(cameraZ);
+        if (Floats.equals(cameraZLen, 0)) {
+            throw new RuntimeException("Camera target vector equals camera position");
+        }
+
+        final Vector3 cameraX;
+        final Vector3 xCandidate = Vec3Math.cross(cameraZ, VECTOR_J);
+        if (Floats.equals(Vec3Math.len(xCandidate) / cameraZLen, 0)) {
+            cameraX = Vec3Math.cross(cameraZ, VECTOR_I);
+        } else {
+            cameraX = xCandidate;
+        }
+
+        final Vector3 cameraY = Vec3Math.cross(cameraZ, cameraX);
+
+        Vec3Math.normalize(cameraX);
+        Vec3Math.normalize(cameraY);
+        Vec3Math.normalize(cameraZ);
+
+        Vector3 cameraPos = position;
+        float cameraXProj = -Vec3Math.dot(cameraX, cameraPos);
+        float cameraYProj = -Vec3Math.dot(cameraY, cameraPos);
+        float cameraZProj = -Vec3Math.dot(cameraZ, cameraPos);
+
+        // YOLO
+
+        view.set(Matrix4Row.R0, Matrix4Col.C0, cameraX.x());
+        view.set(Matrix4Row.R0, Matrix4Col.C1, cameraY.x());
+        view.set(Matrix4Row.R0, Matrix4Col.C2, cameraZ.x());
+
+        view.set(Matrix4Row.R1, Matrix4Col.C0, cameraX.y());
+        view.set(Matrix4Row.R1, Matrix4Col.C1, cameraY.y());
+        view.set(Matrix4Row.R1, Matrix4Col.C2, cameraZ.y());
+
+        view.set(Matrix4Row.R2, Matrix4Col.C0, cameraX.z());
+        view.set(Matrix4Row.R2, Matrix4Col.C1, cameraY.z());
+        view.set(Matrix4Row.R2, Matrix4Col.C2, cameraZ.z());
+
+        view.set(Matrix4Row.R3, Matrix4Col.C0, cameraXProj);
+        view.set(Matrix4Row.R3, Matrix4Col.C1, cameraYProj);
+        view.set(Matrix4Row.R3, Matrix4Col.C2, cameraZProj);
+    }
+
+    public Matrix4 view() {
+        return view;
+    }
+}
