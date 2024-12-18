96c4e78091da804d994376dca88403013382883c
Traunin <traunin5@gmail.com>
2024-12-18 09:52:38 +0300

    feat: camera controls and expose models

--- lib/src/main/java/io/github/nonmilk/coffee/grinder/render/Scene.java
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/render/Scene.java
@@ -1,5 +1,6 @@
 package io.github.nonmilk.coffee.grinder.render;
 
+import io.github.nonmilk.coffee.grinder.Model;
 import io.github.nonmilk.coffee.grinder.camera.Camera;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,13 +9,22 @@ import java.util.List;
 public class Scene {
     
     private final List<Camera> cameras = new ArrayList<>();
+    private Camera selectedCamera;
+    private final List<Model> models = new ArrayList<>();
 
+    public void selectCamera(final int cameraIndex) {
+        selectedCamera = cameras.get(cameraIndex);
+    }
 
-    public void addModel() {
-
+    public Camera selectedCamera() {
+        return selectedCamera;
     }
 
     public List<Camera> cameras() {
         return cameras;
+    }
+
+    public List<Model> models() {
+        return models;
     }
 }
