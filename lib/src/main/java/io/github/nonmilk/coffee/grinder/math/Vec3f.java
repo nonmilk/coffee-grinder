03524cc05afb10fc54a6a3d6db3f0fdbf7c21afd
Traunin <traunin5@gmail.com>
2024-12-20 14:26:14 +0300

    feat: implement Point2f

--- lib/src/main/java/io/github/nonmilk/coffee/grinder/math/Vec3f.java
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/math/Vec3f.java
@@ -1,10 +1,11 @@
 package io.github.nonmilk.coffee.grinder.math;
 
 import io.github.alphameo.linear_algebra.vec.Vector3;
+import io.github.shimeoki.jfx.rasterization.geom.Point2f;
 import io.github.shimeoki.jshaper.obj.geom.ObjVertex;
 
 // rename?
-public class Vec3f implements Vector3 {
+public class Vec3f implements Vector3, Point2f {
     public final static Vec3f VECTOR_I = new Vec3f(1, 0, 0);
     public final static Vec3f VECTOR_J = new Vec3f(0, 1, 0);
     public final static Vec3f VECTOR_K = new Vec3f(0, 0, 1);
