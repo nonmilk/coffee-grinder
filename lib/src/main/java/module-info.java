5deed2de9d77a9c2d2539369442d4df27c9ff622
Traunin <traunin5@gmail.com>
2024-12-21 09:25:39 +0300

    build: add javafx as transitive dependency

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module io.github.nonmilk.coffee {
     requires transitive io.github.shimeoki.jshaper;
     requires io.github.traunin.triangulation;
     requires transitive io.github.alphameo.linear_algebra;
+    requires transitive javafx.graphics;
 
     exports io.github.nonmilk.coffee.grinder;
 }
