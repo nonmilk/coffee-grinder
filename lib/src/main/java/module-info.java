6726b5febcccd2e20e65aaed9f5fc38e4b4cce85
shimeoki <shimeoki@gmail.com>
2024-12-24 14:47:42 +0300

    style: remove comments and organize module-info
    i think it's better to organize the module-info by the keyword. also,
    the comments are doing nothing in this case

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,13 +1,9 @@
 module io.github.nonmilk.coffee.grinder {
 
-    // javafx
-    requires transitive javafx.graphics;
-
-    // not transitive
     requires io.github.shimeoki.jfx.rasterization;
     requires io.github.traunin.triangulation;
 
-    // transitive
+    requires transitive javafx.graphics;
     requires transitive io.github.shimeoki.jshaper;
     requires transitive io.github.alphameo.linear_algebra;
 
