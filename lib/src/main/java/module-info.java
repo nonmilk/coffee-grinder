794fa3941062163d864d7d2f914b8a75c0bfef88
IA1phaI <pavel.2014.play@gmail.com>
2024-12-25 19:23:50 +0300

    build: make external lib with affines requires transitive

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -2,11 +2,11 @@ module io.github.nonmilk.coffee.grinder {
 
     requires io.github.shimeoki.jfx.rasterization;
     requires io.github.traunin.triangulation;
-    requires ru.vsu.cs.konygina_d.render_engine;
 
     requires transitive javafx.graphics;
     requires transitive io.github.shimeoki.jshaper;
     requires transitive io.github.alphameo.linear_algebra;
+    requires transitive ru.vsu.cs.konygina_d.render_engine;
 
     exports io.github.nonmilk.coffee.grinder;
 }
