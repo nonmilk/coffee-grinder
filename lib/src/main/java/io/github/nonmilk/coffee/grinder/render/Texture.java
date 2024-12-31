856f7823c45da3a577f4e7deaf03e03bb2a370b5
Traunin <traunin5@gmail.com>
2024-12-31 18:56:17 +0300

    feat: texture interface

---
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/render/Texture.java
@@ -0,0 +1,7 @@
+package io.github.nonmilk.coffee.grinder.render;
+
+import io.github.shimeoki.jfx.rasterization.color.Colorf;
+
+public interface Texture {
+        public Colorf pixelColor(final float x, final float y);
+}
