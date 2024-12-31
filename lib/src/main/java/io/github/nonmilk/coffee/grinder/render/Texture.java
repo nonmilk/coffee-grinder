db740838c63ebed629a536bffd4441232b642c3c
Traunin <traunin5@gmail.com>
2024-12-31 19:09:11 +0300

    style: remove tab

--- lib/src/main/java/io/github/nonmilk/coffee/grinder/render/Texture.java
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/render/Texture.java
@@ -3,5 +3,5 @@ package io.github.nonmilk.coffee.grinder.render;
 import io.github.shimeoki.jfx.rasterization.color.Colorf;
 
 public interface Texture {
-        public Colorf pixelColor(final float x, final float y);
+    public Colorf pixelColor(final float x, final float y);
 }
