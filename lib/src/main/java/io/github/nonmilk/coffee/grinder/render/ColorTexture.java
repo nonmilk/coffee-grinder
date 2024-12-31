5fa38aa2b0903adb773d21445f705ab12c6fa826
Traunin <traunin5@gmail.com>
2024-12-31 19:02:12 +0300

    feat: Texture implementation for static colors

---
+++ lib/src/main/java/io/github/nonmilk/coffee/grinder/render/ColorTexture.java
@@ -0,0 +1,16 @@
+package io.github.nonmilk.coffee.grinder.render;
+
+import io.github.shimeoki.jfx.rasterization.color.Colorf;
+
+public class ColorTexture implements Texture{
+    private final Colorf color;
+
+    public ColorTexture(Colorf color) {
+        this.color = color;
+    }
+
+    @Override
+    public Colorf pixelColor(final float x, final float y) {
+        return color;
+    }
+}
