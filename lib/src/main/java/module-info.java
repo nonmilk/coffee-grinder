185fbb2a6a4bd8ea8f7977e09ee16e50516d74a5
shimeoki <shimeoki@gmail.com>
2025-01-10 03:30:50 +0300

    build: add all packages to module-info

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,12 +1,22 @@
 module io.github.nonmilk.coffee.grinder {
 
-    requires io.github.shimeoki.jfx.rasterization;
-    requires io.github.traunin.triangulation;
-    requires java.desktop;
+    requires transitive io.github.shimeoki.jfx.rasterization;
+    requires transitive io.github.traunin.triangulation;
+    requires transitive java.desktop;
 
     requires transitive javafx.graphics;
     requires transitive io.github.shimeoki.jshaper;
     requires transitive io.github.alphameo.linear_algebra;
 
     exports io.github.nonmilk.coffee.grinder;
+    exports io.github.nonmilk.coffee.grinder.transformations;
+
+    exports io.github.nonmilk.coffee.grinder.camera;
+    exports io.github.nonmilk.coffee.grinder.camera.view;
+
+    exports io.github.nonmilk.coffee.grinder.math;
+    exports io.github.nonmilk.coffee.grinder.math.affine;
+
+    exports io.github.nonmilk.coffee.grinder.render;
+    exports io.github.nonmilk.coffee.grinder.render.triangle;
 }
